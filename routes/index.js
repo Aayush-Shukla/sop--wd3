509b55afbc4037e97a69578fb2228e1e9c4ab4ec
Ayush Shukla <34869001+Aayush-Shukla@users.noreply.github.com>
2020-05-30 22:36:18 +0530

    feat: add search

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/',authenticationMiddleware (), function(req, res, next) {
     profileid=req.session.passport.user.user_id
 
     const db=require('../db.js')
-    db.query("SELECT content,name FROM users INNER JOIN post ON users.id=post.author_id INNER JOIN followers ON (followers.user_id = users.id OR followers.follower_id=users.id) WHERE followers.user_id=(?) OR followers.follower_id=(?) ORDER BY created_at DESC", [profileid,profileid], function (error, results, fields) {
+    db.query("select author_id,content,name,created_at from post join users on post.author_id = users.id where author_id=(?) union select follower_id,content,name,created_at from followers join post on followers.follower_id=post.author_id join users on post.author_id = users.id where user_id=(?) ", [profileid,profileid], function (error, results, fields) {
         if (error) {
             console.log(error, 'dbquery');
         }
@@ -27,7 +27,7 @@ router.get('/',authenticationMiddleware (), function(req, res, next) {
             }
 
 
-
+            console.log(results)
             res.render('home', {data:{feed:results, name:namese[0].name}});
         })
 
@@ -45,6 +45,15 @@ router.get('/register',checkNotAuthenticated(), function(req, res, next) {
   res.render('register', { title: 'Registration' });
 });
 
+router.get('/search',authenticationMiddleware(), function(req, res, next) {
+  res.render('search', { title: 'Registration' });
+});
+
+
+router.get('/create',authenticationMiddleware(), function(req, res, next) {
+  res.render('create');
+});
+
 router.get('/profile',authenticationMiddleware (), function(req, res, next) {
     authorid=req.session.passport.user.user_id
 
@@ -57,9 +66,15 @@ router.get('/profile',authenticationMiddleware (), function(req, res, next) {
             if (error) {
                 console.log(error, 'dbquery');
             }
-                console.log(profinfo)
+                console.log(results)
+            db.query("SELECT * FROM users WHERE id =(?) ", [authorid], function (error, userdetail, fields) {
+                if (error) {
+                    console.log(error, 'dbquery');
+                }
+                console.log(userdetail[0])
 
-        res.render('profile', {data:{userblogs: results,info:profinfo[0]}})
+                res.render('profile', {data: {userblogs: results, info: profinfo[0],userinfo: userdetail[0]}})
+            })
         })
     })
 
@@ -67,7 +82,7 @@ router.get('/profile',authenticationMiddleware (), function(req, res, next) {
 
 
 });
-router.post('/profile',authenticationMiddleware (), function(req, res, next) {
+router.post('/create',authenticationMiddleware (), function(req, res, next) {
 
 
 
@@ -82,14 +97,7 @@ router.post('/profile',authenticationMiddleware (), function(req, res, next) {
             }
             console.log("success")
         })
-        db.query("SELECT * FROM post WHERE author_id =(?) ORDER BY created_at DESC", [authorid], function (error, results, fields) {
-                    if (error) {
-                        console.log(error,'dbquery');
-                    }
-            res.render('profile', { userblogs: results });
-
-
-        })
+        res.redirect('/')
 
 
 
@@ -100,18 +108,55 @@ router.post('/profile',authenticationMiddleware (), function(req, res, next) {
 router.get('/login',checkNotAuthenticated(), function(req, res, next) {
   res.render('login', { title: 'login' });
 });
+
+
+
+
 router.get('/user/:name', function(req, res, next) {
 
-    const db=require('../db.js')
+
     profilevisit=req.params.name
-    db.query("SELECT content FROM users u JOIN post p on u.id = p.author_id WHERE name= (?) ORDER BY created_at DESC", [profilevisit], function (error, results, fields) {
+    currentuser=req.session.passport.user.user_id
+
+    const db=require('../db.js')
+    db.query("SELECT id FROM users WHERE name =(?) ", [profilevisit], function (error, profileid, fields) {
         if (error) {
             console.log(error, 'dbquery');
         }
-        console.log(results)
-        res.render('profile', {userblogs: results});
-    })
+        // console.log(profileid[0].id)
+        db.query("SELECT (Select count(*) from post where author_id=(?))as postno, (SELECT count(*) from followings where user_id=(?))as followers,(SELECT count(*) from followers where user_id=(?))as followings,(select name from users where id=(?))as name", [profileid[0].id,profileid[0].id,profileid[0].id,profileid[0].id], function (error, profinfo, fields) {
+            if (error) {
+                console.log(error, 'dbquery');
+            }
+            db.query("SELECT content,created_at FROM post WHERE author_id =(?) ", [profileid[0].id], function (error, results, fields) {
+                if (error) {
+                    console.log(error, 'dbquery');
+                }
 
+                db.query("SELECT * from followers where user_id=(?) and follower_id=(?) ", [currentuser,profileid[0].id], function (error, followcheck, fields) {
+                    if (error) {
+                        console.log(error, 'dbquery');
+                    }
+
+                    console.log(followcheck.length==0,'cndn')
+                    if(profileid[0].id==currentuser)
+                    {
+                        res.redirect('/profile')
+                    }
+                    else{
+                    if (followcheck.length==0){
+                        res.render('profile', {data: {userblogs: results, info: profinfo[0], id: profileid[0].id, alreaedyfollow:false}})
+
+                    }
+                    else {
+
+
+                        res.render('profile', {data: {userblogs: results, info: profinfo[0], id: profileid[0].id, alreadyfollow:true}})
+                    }}
+                })
+            })
+        })
+    })
 
 });
 
@@ -149,9 +194,49 @@ router.get('/follow/:id',authenticationMiddleware (), function(req, res, next) {
 
 });
 
+
+
+
+
+router.get('/unfollow/:id',authenticationMiddleware (), function(req, res, next) {
+
+    const db=require('../db.js')
+    userid=req.session.passport.user.user_id
+    tofollow=req.params.id
+    db.query("DELETE FROM followings WHERE following_id=(?) AND user_id=(?)", [userid,tofollow], function (error, results, fields) {
+        if (error) {
+            console.log(error, 'dbquery');
+        }
+        console.log(results)
+    })
+    db.query("DELETE FROM followers WHERE follower_id=(?) AND user_id=(?)", [tofollow,userid], function (error, results, fields) {
+        if (error) {
+            console.log(error, 'dbquery');
+        }
+        console.log(results,'del')
+    })
+
+    db.query("SELECT name FROM users WHERE id=(?)", [tofollow], function (error, results, fields) {
+        if (error) {
+            console.log(error, 'dbquery');
+        }
+        var redirect=results[0].name
+        console.log(redirect)
+        res.redirect(`/user/${redirect}`);
+
+    })
+
+
+
+});
+
+
+
+
+
 router.post('/login', passport.authenticate(
     'local',{
-    successRedirect:'/profile',
+    successRedirect:'/',
     failureRedirect:'/login',
     failureFlash : true
     }));
@@ -228,6 +313,44 @@ router.post('/register', check('username').not().isEmpty().withMessage('name can
 
 
 });
+
+
+
+
+
+
+
+
+router.post('/search',authenticationMiddleware (), function(req, res, next) {
+
+
+
+    search='%'+req.body.search+'%'
+
+
+    const db=require('../db.js')
+
+    db.query("SELECT name,id FROM users WHERE name LIKE (?)", [search], function (error, results, fields) {
+        if (error) {
+            console.log(error,'dbquery');
+        }
+        console.log(results)
+        res.render('search',{search_res:results})
+
+
+    })
+
+
+
+
+
+});
+
+
+
+
+
+
 passport.serializeUser(function(user_id, done) {
     done(null, user_id);
 });
